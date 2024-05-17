96d6d96200749f2d18368a62a31467be2234bf62
LauraAguiar7 <laura.aguiar7@hotmail.com>
2024-05-17 15:50:30 -0300

    atividade senai

--- js/jquery-script.js
+++ js/jquery-script.js
@@ -1,1 +1,12 @@
 //efeito de esconder formulário de cadastro
+
+$(document).ready(function(){
+
+    $("#botao-cadastrar").click(function(){
+
+        $("#form-cadastrar").sliderToggle("slow");
+        $("#section-login").sliderToggle("slow");
+        $("#botao-cadastrar").hide();
+    });
+
+});
